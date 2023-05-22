17fd7789ae64a6eb034e840a569b37e9a71b500c
SulienP <112190974+SulienP@users.noreply.github.com>
2023-05-22 18:02:18 +0200

    changement de la version de maven

---
+++ projet/src/main/java/com/tamgotchi/GameManager.java
@@ -0,0 +1,21 @@
+package com.tamgotchi;
+
+import java.nio.file.*;
+
+public class GameManager {
+    public void gamemanager() {
+        Path path = Path.of("./SaveTamagotchi.jav");
+        if (Files.exists(path)) {
+            /*
+              TODO récupération du tama
+             */
+
+        } else {
+            /*
+             *TODO Lancement du jeu
+             */
+        }
+    } 
+
+      
+}
