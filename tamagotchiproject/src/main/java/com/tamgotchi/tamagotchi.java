d60e7caa467080aa3d13a8d8cd9cd0f695a171ad
arthurLacombe <arthurlacombe44@gmail.com>
2023-05-22 16:27:08 +0200

    add methode cure and complite default values

--- tamagotchiproject/src/main/java/com/tamgotchi/tamagotchi.java
+++ tamagotchiproject/src/main/java/com/tamgotchi/tamagotchi.java
@@ -1,15 +1,20 @@
 package com.tamgotchi;
 
 public class tamagotchi {
-    String name;
-    int age;
-    int food;
-    int health;
-    int happyness;
-    int care;
-    int state;
-    boolean isAlive;
-    int tiredness;
+    String name = "";
+    int age = 0;
+    int food = 30;
+    int health = 50;
+    int happyness = 30;
+    int care = 30;
+    int state = 0;
+    boolean isAlive = true;
+    int tiredness = 20;
+    boolean sick = false;
+
+    tamagotchi(String name){
+        this.name=name;
+    }
     
     public void toilet(){
         //lanch little game who return value between 1 to 100
@@ -20,5 +25,9 @@ public class tamagotchi {
     public void feed(){
         //lanch little game who return value between 1 to 100
         this.food+= 30; //complit with value return by game
+    }
+
+    public void cure(){ //add exception if tamagotchi isn't old
+        this.sick=false;
     }
 }
