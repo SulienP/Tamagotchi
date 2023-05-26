4f7e1109fad3279c13d909a3dcdec25585a3c996
Evan Lb <evan.lebourhis@ynov.com>
2023-05-26 16:22:33 +0200

    création des menu + ajout des menues dans el gameManager + recupération des information

--- projet/src/main/java/com/tamgotchi/GameManager.java
+++ projet/src/main/java/com/tamgotchi/GameManager.java
@@ -6,14 +6,28 @@ public class GameManager {
     public void gamemanager() {
 
         Path path = Path.of("./SaveTamagotchi.jav");
+        
         if (Files.exists(path)) {
             /*
               TODO récupération du tama
              */
 
         } else {
+            System.out.println("Bienvenue dans Tamagotchi");
             Menu menu = new Menu();
-            menu.MenuPlay();
+            String value = menu.MenuPlay();
+            Tamagotchi tamagotchimenu = new Tamagotchi();
+            
+            if (value == "feed") {
+                tamagotchimenu.feed();
+                System.out.println("Vous avez nourri votre Tamagotchi");
+            } else if (value == "wash") {
+                tamagotchimenu.toilet();
+                System.out.println("Vous avez lavé votre Tamagotchi");
+            } else if (value == "heal") {
+                tamagotchimenu.cure();
+                System.out.println("Vous avez soigné votre Tamagotchi");
+            } 
         }
     } 
 
--- projet/src/main/java/com/tamgotchi/tamagotchi.java
+++ projet/src/main/java/com/tamgotchi/tamagotchi.java
@@ -1,5 +1,9 @@
 package com.tamgotchi;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Tamagotchi {
     String name = "";
     int age = 0;
@@ -12,15 +16,37 @@ public class Tamagotchi {
     int tiredness = 20;
     boolean sick = false;
 
+    void askeName() {
+        System.out.println("Qu'elle sera son nom?");
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader buffer = new BufferedReader(reader);
+        String input = buffer.toString();
+        try {
+            input = buffer.readLine();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            askeName();
+        }
+        if (input.isEmpty()) {
+            try {
+                throw new ExceptionTamagotchi(TamagotchiExecptionType.NoName, "noName");
+            } catch (ExceptionTamagotchi e) {
+                System.out.println(e.getMessage());
+                askeName();
+
+            }
+        }
+        tamagotchi(input);
+    }
      
-    void tamagotchi(String name){
+    void tamagotchi(String name) {
+        System.out.println(name);
         this.name=name;
     }
     
     public void toilet(){
         //lanch little game who return value between 1 to 100
         this.care+=30; //complit with value return by game
-       
     }
 
     public void feed(){
