5b5f8bee3354aeafa82650a026b5929590a2b8d6
Evan Lb <evan.lebourhis@ynov.com>
2023-06-05 14:42:15 +0200

    new project

---
+++ project/src/main/java/com/tamagotchi/App.java
@@ -0,0 +1,14 @@
+package com.tamgotchi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) 
+    {  
+        GameManager gameManager = new GameManager();
+        gameManager.gamemanager();
+    }
+}
---
+++ project/src/main/java/com/tamagotchi/Tamagotchi.java
@@ -0,0 +1,62 @@
+package com.tamgotchi;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Tamagotchi {
+    String name = "";
+    int age = 0;
+    int food = 30;
+    int health = 50;
+    int happyness = 30;
+    int care = 30;
+    int state = 0;
+    boolean isAlive = true;
+    int tiredness = 20;
+    boolean sick = false;
+
+    void askeName() {
+        System.out.println("Qu'elle sera son nom?");
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader buffer = new BufferedReader(reader);
+        String input = buffer.toString();
+        try {
+            input = buffer.readLine();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            askeName();
+        }
+        if (input.isEmpty()) {
+            try {
+                throw new ExceptionTamagotchi(TamagotchiExecptionType.NoName, "noName");
+            } catch (ExceptionTamagotchi e) {
+                System.out.println(e.getMessage());
+                askeName();
+
+            }
+        }
+        tamagotchi(input);
+        Clear clear = new Clear();
+        clear.clearConsole();
+    }
+     
+    void tamagotchi(String name) {
+        System.out.println(name);
+        this.name=name;
+    }
+    
+    public void toilet(){
+        //lanch little game who return value between 1 to 100
+        this.care+=30; //complit with value return by game
+    }
+
+    public void feed(){
+        //lanch little game who return value between 1 to 100
+        this.food+= 30; //complit with value return by game
+    }
+
+    public void cure(){ //add exception if tamagotchi isn't old
+        this.sick=false;
+    }
+}
